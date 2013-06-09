27e8f4fa3357e71c92bf93c0631967899b08c4e5
lucas <mail@limitlesschannels.com>
2013-06-08 19:31:25 -0700

    added linkedin to me page, added code category, still fixing plugin

--- _plugins/gitactivity.rb
+++ _plugins/gitactivity.rb
@@ -5,11 +5,25 @@
 #
 
 require 'git'
+# require 'maxlength'
 
 module Jekyll
-  class GitActivityTag < Liquid::Tag
 
-    #priority :high
+  class String
+
+    def maxlength(ceiling=80)
+      in_string = self
+      if in_string.length > ceiling
+        # minus 3 for ellipsis
+        out_string = in_string.slice(0..(ceiling-3))
+        return out_string+'...'
+      else
+        return in_string
+      end
+    end
+  end
+
+  class GitActivityTag < Liquid::Tag
 
     def initialize(tag_name, text, tokens)
       super
@@ -18,7 +32,7 @@ module Jekyll
     def render(context)
       result = ""
       git_base = Git.open(File.join(Dir.getwd, "."))
-      commit_url = git_base.remote.url.gsub(/\.git/, '/commit/')
+      commit_url = git_base.remote.url.gsub(/\.git/, %q{/commit/})
       displayed_commits = 2
 
       git_base.log(displayed_commits).each do |commit|
@@ -27,7 +41,7 @@ module Jekyll
           result << " - <a href='#{commit_url}"
           result << commit.sha
           result << "/'>"
-          result << commit.message.slice(0..100)+'...'
+          result << commit.message.maxlength
           result << "</a></li>"
 
       end
