753a43cfb44c7bb20ac48ec49cedd5de911aa574
lucas <mail@limitlesschannels.com>
2013-06-08 17:18:21 -0700

    added github commit plugin to sidebar

---
+++ _plugins/git.rb
@@ -0,0 +1,37 @@
+#
+# title:  git-activity plugin for jekyll.rb
+# author: theoretick
+# credit: modified code and original idea from github.com/alx
+#
+
+require 'git'
+
+module Jekyll
+  class GitActivityTag < Liquid::Tag
+
+    def initialize(tag_name, text, tokens)
+      super
+    end
+
+    def render(context)
+      result = ""
+      git_base = Git.open(File.join(Dir.getwd, "."))
+      commit_url = git_base.remote.url.gsub(/\.git/, '/commit/')
+      displayed_commits = 2
+
+      git_base.log(displayed_commits).each do |commit|
+          result << "<li>"
+          result << commit.date.strftime("%d%B")
+          result << " - <a href='#{commit_url}"
+          result << commit.sha
+          result << "/'>"
+          result << commit.message
+          result << "</a></li>"
+
+      end
+      return "<ul>#{result}</ul>"
+    end
+  end
+end
+
+Liquid::Template.register_tag('gitactivity', Jekyll::GitActivityTag)
